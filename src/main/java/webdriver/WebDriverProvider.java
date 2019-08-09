d79e81d75a5a6d4645d5a28641466270afaac7bc
Natallia Dzemianok <natallia_dzemianok@epam.com>
2019-08-09 18:07:53 +0300

    add WD

---
+++ src/main/java/webdriver/WebDriverProvider.java
@@ -0,0 +1,25 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.Arrays;
+
+public class WebDriverProvider {
+
+	public static WebDriver driverInstance;
+	
+	public static WebDriver getDriverInstance()
+	 {
+	  if (driverInstance == null)
+	  {
+		  ChromeOptions chromeOptions = new ChromeOptions();
+		  chromeOptions.addArguments(Arrays.asList("--disable-web-security", "--allow-running-insecure-content", "enable-automation", "--no-sandbox", "--disable-extensions", "--dns-prefetch-disable", "--disable-gpu"));
+
+		  driverInstance = new ChromeDriver(chromeOptions);
+	  }
+	  return driverInstance;
+	 }
+}
+
